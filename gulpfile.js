f93f408a24665fd68589b46018cd0f6850a29baf
Ryan Neil Stroud <ryan@boutir.com>
2017-07-22 12:51:39 +0800

    Structured folders.

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./src/sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./styles'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
